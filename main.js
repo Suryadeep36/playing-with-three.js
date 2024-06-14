3fb46bfce8adc499a9cdcc9627021e50d4d515b7
Suryadeep36 <gohilsuryadeep3101@gmail.com>
2024-06-14 23:35:30 +0530

    add camera, renderer, scene and a BLUE cube boii

---
+++ main.js
@@ -0,0 +1,25 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry(5 ,5, 20 );
+const material = new THREE.MeshBasicMaterial( { color: "blue" } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 20;
+
+
+function animate() {
+
+	cube.rotation.x += 0.1;
+	cube.rotation.y += 0.02;
+	renderer.render( scene, camera );
+
+}
