0c8f516ebdb58928b7d9068b63874cbc72fe5ccd
Suryadeep36 <gohilsuryadeep3101@gmail.com>
2024-06-15 23:56:28 +0530

    make canvas as a background so i can add content on it

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+	canvas: document.querySelector('#bg')
+});
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
+// document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry(5 ,5, 20 );
 const material = new THREE.MeshBasicMaterial( { color: "blue" } );
